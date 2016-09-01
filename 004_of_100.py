8ef09ed34cc1d37f82d6045f8de2288929a3fb1e
Sunwei <sansunwei@163.com>
2016-09-01 22:12:35 +0800

    004 add java

--- 004_of_100.py
+++ 004_of_100.py
@@ -61,7 +61,31 @@ sum = sum + b(floor(a2/100)) + mod(a2,100);
 %        date 当前日期字符串
 %        now 当前日期和时间的序列数
 
+# Filename:J_004_of_100.java
+package one_of_100;
+import java.io.*;
+public class J_004_of_100 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        String str = null; 
+        System.out.println("Please input the datatime: (20160901): "); 
+        str = br.readLine();
+       
+		int sum = 0 ;
+//		i = '20160901';
+		int[] b= {0 ,31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+		
+		int a1 = Integer.parseInt((str.substring(1, 5)));
+		int a2 = Integer.parseInt((str.substring(5)));
+		if( a1%4==0){
+		    sum = sum+1;
+		}
+		sum = sum + b[a2/100 - 1] +a2%100;
+		System.out.println(sum);
+	}
+}
 
-
-
-
+// IO口要有  throws IOException
+Java 数组 从 0 下标开始
+Java 可以直接用 / % 求余 求商
